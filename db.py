6358523acd2eb7e1747e0117b810201d27477053
Xu Yaosheng <xysss.jason@gmail.com>
2022-11-28 09:23:28 -0600

    first commit, 1.get all data. 2.use flask api

---
+++ db.py
@@ -0,0 +1,32 @@
+from flask import Flask
+import pymysql
+# from flask_cors import *
+# from flask import Response,request
+
+app = Flask(__name__)
+# CORS(app, supports_credentials=True)
+
+
+class Database:
+
+  def __init__(self):
+    host = "34.66.155.131"
+    user = "root"
+    password = "CS410project"
+    db = "cs410"
+    self.con = pymysql.connect(host=host,
+                               user=user,
+                               password=password,
+                               db=db,
+                               cursorclass=pymysql.cursors.DictCursor)
+    self.cur = self.con.cursor()
+
+#   def test(self):
+#     sql = "select * from testDB.Unit"
+#     self.cur.execute(sql)
+#     result = self.cur.fetchall()
+#     return result
+
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
---
+++ main.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request, redirect
+import db
+
+app = Flask(__name__)
+mydb = db.Database()
+
+
+@app.route('/')
+def index():
+  return render_template('index.html')
+
+
+@app.route('/search_user', methods=['POST'])
+def search_user():
+  firstName = request.form.get('fname')
+  lastName = request.form.get('lname')
+  email = request.form.get('email')
+  result = mydb.search_user(firstName, lastName, email)
+  return render_template('users.html', result=result)
+
+
+@app.route('/test_db/')
+def test_db():
+  result = mydb.test()
+  return result
+
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=81, debug=True)
